0a3d6457106ceaf53860a3d2ac76733f5bad03fd
Piotr Kaźmierski <217847@edu.p.lodz.pl>
2021-04-19 20:51:22 +0200

    Update ExercisePackageObjectTest

--- src/test/scala/exercise/grades/ExercisePackageObjectTest.scala
+++ src/test/scala/exercise/grades/ExercisePackageObjectTest.scala
@@ -7,7 +7,7 @@ class ExercisePackageObjectTest extends FunSuite {
 	test("average value exercise test") {
 		// porada: można szybko zsumować liczby w liście korzystając za pomocą metody 'sum', np. mojaLista.sum
 		
-		// assert(grades.size == 3)
-		// assert(calculateAverage(grades) == 4.0)
+//		 assert(grades.size == 3) // lista lub inna kolekcja
+//		 assert(calculateFinalGrade(grades) == 4)
 	}
 }
